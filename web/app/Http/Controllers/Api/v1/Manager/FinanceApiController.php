bab91b5a31b13506c4e732e4fdb6def8ec0a39c8
Alex Oleynik <oleynik.webdev@gmail.com>
2020-10-26 20:25:10 +0200

    finance manager page dates fix;

--- web/app/Http/Controllers/Api/v1/Manager/FinanceApiController.php
+++ web/app/Http/Controllers/Api/v1/Manager/FinanceApiController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers\Api\v1\Manager;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
 use Carbon\Carbon;
+
+use App\Repositories\PlaceRepositoryInterface;
 use App\Models\Order;
 
 class FinanceApiController extends Controller
 {
+    private $placeRepository;
+
+    public function __construct(
+        PlaceRepositoryInterface $placeRepository
+    )
+    {
+        $this->placeRepository = $placeRepository;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -19,63 +29,79 @@ class FinanceApiController extends Controller
     {
         $now = Carbon::now();
 
+        $place = $this->getPlace();
+
+        $query1 = Order::query()
+            ->where('place_id', $place->id)
+            ->status('draft', '!=');
+
         $d1 = $now->copy();
         $res[] = [
             'title' => 'Заказы сегодня:',
             'dates' => [$d1->format('Y-m-d')],
-            'count' => Order::status('draft', '!=')->whereDate('created_at', $d1)->count(),
-            'amount' => Order::status('draft', '!=')->whereDate('created_at', $d1)->sum('amount'),
+            'count' => (clone $query1)->whereDate('created_at', $d1)->count(),
+            'amount' => (clone $query1)->whereDate('created_at', $d1)->sum('amount'),
         ];
-        $d1 = $now->copy()->addDay(-1);
+        $d1 = $now->copy()->addDays(-1);
         $res[] = [
             'title' => 'Заказы вчера:',
             'dates' => [$d1->format('Y-m-d')],
-            'count' => Order::status('draft', '!=')->whereDate('created_at', $d1)->count(),
-            'amount' => Order::status('draft', '!=')->whereDate('created_at', $d1)->sum('amount'),
+            'count' => (clone $query1)->whereDate('created_at', $d1)->count(),
+            'amount' => (clone $query1)->whereDate('created_at', $d1)->sum('amount'),
         ];
         $d1 = $now->copy()->startOfWeek();
         $d2 = $now->copy()->endOfWeek();
         $res[] = [
             'title' => 'За эту неделю:',
             'dates' => [$d1->format('Y-m-d'), $d2->format('Y-m-d')],
-            'count' => Order::status('draft', '!=')
+            'count' => (clone $query1)
                 ->whereBetween('created_at', [$d1, $d2])->count(),
-            'amount' => Order::status('draft', '!=')
+            'amount' => (clone $query1)
                 ->whereBetween('created_at', [$d1, $d2])->sum('amount'),
         ];
-        $d1 = $now->copy()->addDay(-8)->startOfWeek();
-        $d2 = $now->copy()->addDay(-8)->startOfWeek();
+        $d1 = $now->copy()->addDays(-7)->startOfWeek();
+        $d2 = $now->copy()->addDays(-7)->endOfWeek();
         $res[] = [
             'title' => 'За прошлую неделю:',
             'dates' => [$d1->format('Y-m-d'), $d2->format('Y-m-d')],
-            'count' => Order::status('draft', '!=')
+            'count' => (clone $query1)
                 ->whereBetween('created_at', [$d1, $d2])->count(),
-            'amount' => Order::status('draft', '!=')
+            'amount' => (clone $query1)
                 ->whereBetween('created_at', [$d1, $d2])->sum('amount'),
         ];
         $d1 = $now->copy()->startOfMonth();
-        $d2 = $now->copy()->startOfMonth();
+        $d2 = $now->copy()->endOfMonth();
         $res[] = [
             'title' => 'За этот месяц:',
             'dates' => [$d1->format('Y-m-d'), $d2->format('Y-m-d')],
-            'count' => Order::status('draft', '!=')
+            'count' => (clone $query1)
                 ->whereBetween('created_at', [$d1, $d2])->count(),
-            'amount' => Order::status('draft', '!=')
+            'amount' => (clone $query1)
                 ->whereBetween('created_at', [$d1, $d2])->sum('amount'),
         ];
-        $d1 = $now->copy()->addMonth(-1)->startOfMonth();
-        $d2 = $now->copy()->addMonth(-1)->startOfMonth();
+        $d1 = $now->copy()->addMonths(-1)->startOfMonth();
+        $d2 = $now->copy()->addMonths(-1)->endOfMonth();
         $res[] = [
             'title' => 'За прошлый месяц:',
             'dates' => [$d1->format('Y-m-d'), $d2->format('Y-m-d')],
-            'count' => Order::status('draft', '!=')
+            'count' => (clone $query1)
                 ->whereBetween('created_at', [$d1, $d2])->count(),
-            'amount' => Order::status('draft', '!=')
+            'amount' => (clone $query1)
                 ->whereBetween('created_at', [$d1, $d2])->sum('amount'),
         ];
 
         return [
             'data' => $res
         ];
+    }
+
+    private function getPlace($place_id = null)
+    {
+        $place = $place_id
+            ? $this->placeRepository->find($place_id)
+            : $this->placeRepository->findByWhere([['manager_id', '=', \Auth::id()]]);
+        abort_if(!$place || $place->manager_id !== \Auth::id(), 404);
+
+        return $place;
     }
 }
